984b47b0c71c339c065710a34dc3dddc9e111f1a
Zohaib Ramzna <apricopt@gmail.com>
2024-07-19 21:41:27 +0500

    new feature added

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 const configuration = {
     headless : false,
     slowDown: 250, 
-    startingTimeOfDay: 7,   //24 hour clock,
-    endingTimeOfDay: 21,    // 24 hour clock,
-    renewTimeRange: [65, 70]  // 65 to 70 minutes default ( Must be greater than 60)
+    startingTimeOfDay: 7,  //24 hour clock,
+    endingTimeOfDay: 23,    // 24 hour clock,
+    renewTimeRange: [65, 70]   // 65 to 70 minutes default ( Must be greater than 60)
 }
+
+
 
 
 module.exports = {configuration}
